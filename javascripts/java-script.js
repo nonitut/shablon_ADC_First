9df71e17adeef2f94976243368700b5476222a2e
Nona <87380138+nonitut@users.noreply.github.com>
2023-05-27 19:14:03 +0300

    Update java-script.js

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -1,1 +1,6 @@
 //write here your js
+
+document.addEventListener("DOMContentLoaded", function() {
+    // используй эту функцию, для элементов которые должны быть выполнены после загрузки HTML
+    // Это как Document ready, только для vanila js
+});
